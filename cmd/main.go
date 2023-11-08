e897142942124ad04c257f2be0a14a145e91ed1e
Esteban Gomez <unawaretub86@gmail.com>
2023-11-07 19:10:05 -0500

    change

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/unawaretub86/top-secret/internal/handler"
+	"github.com/unawaretub86/top-secret/internal/adapters/handler"
 )
 
 func main() {
